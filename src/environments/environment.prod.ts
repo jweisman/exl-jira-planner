03adc14c3d9d960da61e6bda10f532fe76716159
Josh Weisman <josh.m.weisman@gmail.com>
2020-07-27 15:09:47 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  jiraProxy: "http://il-docker.corp.exlibrisgroup.com:8091",
+  jiraProxy: "http://il-docker01.corp.exlibrisgroup.com:8091",
   jiraUrl: "https://jira.exlibrisgroup.com",
   serviceUrl: "https://api-eu.exldevnetwork.net/jira-planner",
 };
